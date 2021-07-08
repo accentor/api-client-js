31906401147fae16ce803b0cfa5bb311834918ce
Robbe Van Petegem <robbe@vanpetegem.me>
2021-07-08 20:20:56 +0200

    Initialize repository

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    curly: "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+};
