db1c0fc578c12006bc5581958d9dbe0d12d2a535
Robbe Van Petegem <robbe@vanpetegem.me>
2021-08-21 17:18:40 +0200

    Add basic test to fetch albums

---
+++ test/integration/albums.test.ts
@@ -0,0 +1,31 @@
+import { setup, suite, test } from "mocha";
+import { assert } from "chai";
+import fetchMock from "fetch-mock";
+global.fetch = fetchMock.sandbox();
+fetchMock.get(
+  {
+    url: "http://example.org/api/albums?page=1",
+    headers: { "x-secret": "123", "x-device-id": "abc" },
+  },
+  {
+    body: "[]",
+    headers: { "x-total-pages": 1 },
+  }
+);
+
+import { AlbumModule } from "../../src/api_module";
+
+suite("AlbumsModule", function () {
+  let module;
+
+  setup(function () {
+    module = new AlbumModule("http://example.org/api");
+  });
+
+  test("should correctly call index path", async function () {
+    const index = module.index({ device_id: "abc", secret: "123" });
+    const response = await index.next();
+    assert.equal(response.value.length, 0);
+    assert(response.done);
+  });
+});
