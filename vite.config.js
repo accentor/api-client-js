d17cd8ea1fd06ad93855ced0b4a432f8fad32795
Robbe Van Petegem <git@robbevp.be>
2025-03-22 14:37:02 +0100

    Switch build system to vite (#567)
    * Switch build to vite
    
    * Ignore dist files in eslint
    
    * Re-add node-ts, since we still need this
    
    * Enable sourcemaps in output
    
    * Remove `.keep` since vite re-creates the whole `dist/` folder on each build

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
+  build: {
+    sourcemap: true,
+    lib: {
+      name: "@accent/api-client-js",
+      entry: resolve(__dirname, "src/index.ts"),
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["fetch-retry"],
+      output: {
+        globals: {
+          "fetch-retry": "useFetchRetry",
+        },
+      },
+    },
+  },
+});
